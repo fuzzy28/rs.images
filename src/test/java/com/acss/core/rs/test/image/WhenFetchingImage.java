4dbcfafff5e0eba8140ad055805a07c5862c8b14
gvargas <vargas.gail@gmail.com>
2015-10-13 20:50:19 +0800

    Added the Document Followup Module 	-able to fulfill the request from document followup request. 	-upload the requested file. 	-updated the image details into table. 	-added button for view.

--- src/test/java/com/acss/core/rs/test/image/WhenFetchingImage.java
+++ src/test/java/com/acss/core/rs/test/image/WhenFetchingImage.java
@@ -4,6 +4,7 @@ package com.acss.core.rs.test.image;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
+import org.jooq.Condition;
 import org.junit.rules.ExpectedException;
 
 import java.util.List;
@@ -16,6 +17,8 @@ import org.springframework.dao.DataRetrievalFailureException;
 import com.acss.core.model.image.ApplicationImage;
 import com.acss.core.orm.ReadOnlyRepository;
 import com.acss.core.rs.test.BaseTestRunner;
+
+import static com.acss.core.rs.db.tables.TImage.T_IMAGE;
 
 public class WhenFetchingImage extends BaseTestRunner{
 	@Autowired
@@ -39,8 +42,8 @@ public class WhenFetchingImage extends BaseTestRunner{
 		 * IMAGEPATH = /storwyzmnt/batch/imageconverter/fax_images/2870827/201507220001_00002.jpg
 		 * 
 		*/
-		ApplicationImage resultImage = imageRepository.findById(new String("201507220001"));
-		final String default_ImagePath= "/storwyzmnt/batch/imageconverter/fax_images/000000/201507220001_00001.jpg";
+		ApplicationImage resultImage = imageRepository.findById(new String("201507300275"));
+		final String default_ImagePath= "C:\\osa\\app_images\\201507300106\\201507300275.jpg";
 		assertThat("Image should not be null! ",resultImage,is(notNullValue()));
 		assertThat("Imagepath should match the default entry", 
 				resultImage.getImagePath(), is(equalTo(default_ImagePath)));
@@ -58,24 +61,20 @@ public class WhenFetchingImage extends BaseTestRunner{
 	
 	@Test
 	public void shouldReturnRecordOnConditionMet(){
-		//TODO Please add some test implementation about Returning a record when conditon is met.
-		/**
-		Example Test Implementation Guide.
+		//Example Test Implementation Guide.
 		//arrange
-		Condition whereImageEqualToGail = M_ACCOUNT.USERNAME.equal("admin");
-		Condition whereEmailLikeVargas = M_ACCOUNT.EMAIL.like("vargas%");
+		Condition whereDataCd = T_IMAGE.DATACD.eq("100133907-4");
 		
 		int actualSize = 1;
 		//Act
-		List<Image> images = imageRepository
-				.findUsingCondition(whereImageEqualToGail
-						.and(whereEmailLikeVargas));
+		List<ApplicationImage> images = imageRepository
+				.findUsingCondition(whereDataCd);
 		//Arrange
 		assertThat("Image should not be null! ",images,is(notNullValue()));
 		assertThat("Query should return 1 record!", images.size(),is(equalTo(actualSize)));
 		assertThat("Result image's username should be 'admin' !"
-				,images.get(0).getImagename(),is(equalTo("admin")));
-		**/
+				,images.get(0).getDataCd(),is(equalTo("admin")));
+		
 		assert(true);
 	}
 	
